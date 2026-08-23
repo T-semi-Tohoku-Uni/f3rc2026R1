be2a33cf9b888456fb76bb9377fc54b224bd0129
SouTakei <ruriti@moimoi.re>
2026-08-23 15:53:26 +0900

    Initial commit

---
+++ Core/Src/main.c
@@ -0,0 +1,657 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+typedef struct {
+    volatile float gosagoukei;
+    volatile float maenogosa;   
+    float Kp, Ki, Kd;    
+    volatile int16_t v;
+    uint16_t can_id; 
+    volatile int16_t mokuhyou;
+} Motor;
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+FDCAN_HandleTypeDef hfdcan1;
+FDCAN_HandleTypeDef hfdcan3;
+
+TIM_HandleTypeDef htim6;
+#include <math.h> // 数学
+#include <stdlib.h> // 絶対値
+#define PI 3.14159265359
+
+/* USER CODE BEGIN PV */
+
+Motor motors[4];
+int16_t g_shusoku[4] = {0,0,0,0};
+FDCAN_TxHeaderTypeDef TxHeader;
+FDCAN_RxHeaderTypeDef RxHeader;
+
+FDCAN_TxHeaderTypeDef TxHeader_com;
+FDCAN_RxHeaderTypeDef RxHeader_com;
+
+volatile uint8_t sekiyu_state = 0; // 0:上移動待機 1:上移動 2:下移動待機 3:下移動
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM6_Init(void);
+static void MX_FDCAN3_Init(void);
+static void MX_FDCAN1_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+HAL_StatusTypeDef CAN_SEND(uint32_t CANID, uint8_t *txdata, FDCAN_HandleTypeDef *hfdcan, FDCAN_TxHeaderTypeDef *htxheader)
+{
+  htxheader->Identifier = CANID;
+  if (HAL_OK != HAL_FDCAN_AddMessageToTxFifoQ(hfdcan, htxheader, txdata))
+  {
+    printf("addmessage error\r\n");
+    return HAL_ERROR;
+  }
+  return HAL_OK;
+}
+
+
+void motor_CAN_filter_init(FDCAN_FilterTypeDef *Hfdcan_Filter_Settings)
+{
+  Hfdcan_Filter_Settings->IdType = FDCAN_STANDARD_ID;
+  Hfdcan_Filter_Settings->FilterIndex = 0;
+  Hfdcan_Filter_Settings->FilterType = FDCAN_FILTER_RANGE;
+  Hfdcan_Filter_Settings->FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
+  Hfdcan_Filter_Settings->FilterID1 = 0x200;
+  Hfdcan_Filter_Settings->FilterID2 = 0x410;
+}
+
+void motor_CAN_txheader_init(FDCAN_TxHeaderTypeDef *Htxheader)
+{
+  Htxheader->Identifier = 0x200;
+  Htxheader->IdType = FDCAN_STANDARD_ID;
+  Htxheader->TxFrameType = FDCAN_DATA_FRAME;
+  Htxheader->DataLength = FDCAN_DLC_BYTES_8;
+  Htxheader->ErrorStateIndicator = FDCAN_ESI_ACTIVE;
+  Htxheader->FDFormat = FDCAN_CLASSIC_CAN;
+  Htxheader->BitRateSwitch = FDCAN_BRS_ON;
+  Htxheader->TxEventFifoControl = FDCAN_NO_TX_EVENTS;
+  Htxheader->MessageMarker = 0;
+}
+
+HAL_StatusTypeDef motor_CAN_RxTxSettings_init(FDCAN_TxHeaderTypeDef *Htxheader)
+{
+  FDCAN_FilterTypeDef FDCAN_Filter_settings;
+  motor_CAN_filter_init(&FDCAN_Filter_settings);
+  motor_CAN_txheader_init(Htxheader);
+  if (HAL_OK != HAL_FDCAN_ConfigFilter(&hfdcan3, &FDCAN_Filter_settings))
+  {
+    printf("fdcan_configfilter is error\r\n");
+    return HAL_ERROR;
+  }
+  if (HAL_OK != HAL_FDCAN_ConfigGlobalFilter(&hfdcan3, FDCAN_REJECT, FDCAN_FILTER_REJECT, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE))
+  {
+    printf("fdcan_configglobalfilter is error\r\n");
+    return HAL_ERROR;
+  }
+  if (HAL_OK != HAL_FDCAN_Start(&hfdcan3))
+  {
+    printf("fdcan_start is error\r\n");
+    return HAL_ERROR;
+  }
+  if (HAL_OK != HAL_FDCAN_ActivateNotification(&hfdcan3, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0))
+  {
+    printf("fdcan_activatenotification is error\r\n");
+    return HAL_ERROR;
+  }
+
+  return HAL_OK;
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (htim->Instance == TIM6) 
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+  }
+}
+
+// void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs){
+// 	if ((RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) != RESET) {
+
+//     /* Retrieve Rx messages from RX FIFO0 */
+// 		uint8_t RxData_motor[8] = {};
+//     FDCAN_RxHeaderTypeDef RxHeader_motor;
+// 		if (HAL_OK != HAL_FDCAN_GetRxMessage(&hfdcan3, FDCAN_RX_FIFO0, &RxHeader_motor, RxData_motor)) {
+// 			printf("fdcan_getrxmessage_motor is error\r\n");
+// 			Error_Handler();
+// 		}
+// 		/*receive robomas's status*/
+// 		for (int i=0; i < 8; i++){
+// 			if (RxHeader_motor.Identifier == (robomas[i].CANID)) {
+// 				robomas[i].actangle = (int16_t)((RxData_motor[0] << 8) | RxData_motor[1]);
+// 				robomas[i].actVel = (int16_t)((RxData_motor[2] << 8) | RxData_motor[3]);
+// 				robomas[i].actCurrent = (int16_t)((RxData_motor[4] << 8) | RxData_motor[5]);
+// 			}
+// 		}
+// 	}
+// }
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+if(htim == &htim6)
+{
+/* ------------ モーター0 ------------ */
+
+if(HAL_GPIO_ReadPin(GPIOC, motor0_upper_Pin))
+{
+if(motors[0].mokuhyou < 0)
+{
+motors[0].mokuhyou = 0;
+}
+}
+
+if(HAL_GPIO_ReadPin(GPIOC, motor0_lower_Pin))
+{
+if(motors[0].mokuhyou > 0)
+{
+motors[0].mokuhyou = 0;
+}
+}
+
+/* ------------ モーター1 ------------ */
+
+if(HAL_GPIO_ReadPin(GPIOC, motor1_upper_Pin))
+{
+if(motors[1].mokuhyou < 0)
+{
+motors[1].mokuhyou = 0;
+}
+}
+
+if(HAL_GPIO_ReadPin(GPIOC, motor1_lower_Pin))
+{
+if(motors[1].mokuhyou > 0)
+{
+motors[1].mokuhyou = 0;
+}
+}
+
+/* ------------ PID計算 ------------ */
+
+TxHeader.Identifier = 0x200;
+uint8_t TxData[8] = {0};
+
+for(int h = 0; h < 2; h++)
+{
+float current = (float)motors[h].v;
+
+float error =
+(float)motors[h].mokuhyou - current;
+
+motors[h].gosagoukei += error;
+
+if(motors[h].gosagoukei > 100)
+motors[h].gosagoukei = 100;
+
+if(motors[h].gosagoukei < -100)
+motors[h].gosagoukei = -100;
+
+float derivative =
+(error - motors[h].maenogosa) / 0.001f;
+
+float output =
+motors[h].Kp * error
++ motors[h].Ki * motors[h].gosagoukei
++ motors[h].Kd * derivative;
+
+motors[h].maenogosa = error;
+
+if(output > 32767)
+output = 32767;
+
+if(output < -32767)
+output = -32767;
+
+int16_t current_cmd = (int16_t)output;
+
+TxData[2*h] = (uint8_t)(current_cmd >> 8);
+TxData[2*h + 1] = (uint8_t)(current_cmd & 0xFF);
+}
+
+/* 未使用モーターは0 */
+
+TxData[4] = 0;
+TxData[5] = 0;
+TxData[6] = 0;
+TxData[7] = 0;
+
+HAL_FDCAN_AddMessageToTxFifoQ(
+&hfdcan3,
+&TxHeader,
+TxData
+);
+}
+}
+    // リミットスイッチ処理↑
+    
+    // モーター速度フィードバック受信
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan,
+uint32_t RxFifo0ITs)
+{
+if ((RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) != RESET)
+{
+uint8_t RxData[8];
+
+if (HAL_FDCAN_GetRxMessage(&hfdcan3,
+FDCAN_RX_FIFO0,
+&RxHeader,
+RxData) != HAL_OK)
+{
+printf("fdcan_getrxmessage is error\r\n");
+Error_Handler();
+}
+
+for (int i = 0; i < 2; i++)
+{
+if (RxHeader.Identifier == motors[i].can_id)
+{
+// DJIモーター速度(RPM)
+motors[i].v =
+(int16_t)((RxData[2] << 8) | RxData[3]);
+}
+}
+}
+}
+
+// マイコン間通信
+void HAL_FDCAN_RxFifo1Callback(FDCAN_HandleTypeDef *hfdcan,
+uint32_t RxFifo1ITs)
+{
+if ((RxFifo1ITs & FDCAN_IT_RX_FIFO1_NEW_MESSAGE) != RESET)
+{
+uint8_t RxData[8] = {};
+
+if (HAL_FDCAN_GetRxMessage(&hfdcan1,
+FDCAN_RX_FIFO1,
+&RxHeader_com,
+RxData) != HAL_OK)
+{
+printf("fdcan_getrxmessage is error\r\n");
+Error_Handler();
+}
+
+if (RxHeader_com.Identifier == 0x010)
+{
+uint8_t motor0_up = RxData[0];
+uint8_t motor0_down = RxData[1];
+
+uint8_t motor1_up = RxData[2];
+uint8_t motor1_down = RxData[3];
+
+/* motor0 */
+
+if (motor0_down)
+{
+motors[0].mokuhyou = 4000; // 下移動
+}
+else if (motor0_up)
+{
+motors[0].mokuhyou = -4000; // 上移動
+}
+else
+{
+motors[0].mokuhyou = 0;
+}
+
+/* motor1 */
+
+if (motor1_down)
+{
+motors[1].mokuhyou = 4000; // 下移動
+}
+else if (motor1_up)
+{
+motors[1].mokuhyou = -4000; // 上移動
+}
+else
+{
+motors[1].mokuhyou = 0;
+}
+}
+}
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM6_Init();
+  MX_FDCAN3_Init();
+  MX_FDCAN1_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim6);
+  if (HAL_OK != motor_CAN_RxTxSettings_init(&TXHeader_motor)) Error_Handler();
+while(1){
+  // uint8_t txdata[8] = {0};
+  // int16_t cu = -3980;
+  // txdata[0] = (uint8_t)(cu >> 8);
+  // txdata[1] = (uint8_t)(cu & 0xFF);
+  // CAN_SEND(0x200,txdata, &hfdcan3, &TXHeader_motor);
+  // HAL_Delay(100);
+}
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+   
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 10;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief FDCAN1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_FDCAN1_Init(void)
+{
+
+  /* USER CODE BEGIN FDCAN1_Init 0 */
+
+  /* USER CODE END FDCAN1_Init 0 */
+
+  /* USER CODE BEGIN FDCAN1_Init 1 */
+
+  /* USER CODE END FDCAN1_Init 1 */
+  hfdcan1.Instance = FDCAN1;
+  hfdcan1.Init.ClockDivider = FDCAN_CLOCK_DIV1;
+  hfdcan1.Init.FrameFormat = FDCAN_FRAME_FD_BRS;
+  hfdcan1.Init.Mode = FDCAN_MODE_NORMAL;
+  hfdcan1.Init.AutoRetransmission = DISABLE;
+  hfdcan1.Init.TransmitPause = DISABLE;
+  hfdcan1.Init.ProtocolException = DISABLE;
+  hfdcan1.Init.NominalPrescaler = 4;
+  hfdcan1.Init.NominalSyncJumpWidth = 1;
+  hfdcan1.Init.NominalTimeSeg1 = 15;
+  hfdcan1.Init.NominalTimeSeg2 = 4;
+  hfdcan1.Init.DataPrescaler = 2;
+  hfdcan1.Init.DataSyncJumpWidth = 1;
+  hfdcan1.Init.DataTimeSeg1 = 15;
+  hfdcan1.Init.DataTimeSeg2 = 4;
+  hfdcan1.Init.StdFiltersNbr = 1;
+  hfdcan1.Init.ExtFiltersNbr = 0;
+  hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+  if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN FDCAN1_Init 2 */
+
+  /* USER CODE END FDCAN1_Init 2 */
+
+}
+
+/**
+  * @brief FDCAN3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_FDCAN3_Init(void)
+{
+
+  /* USER CODE BEGIN FDCAN3_Init 0 */
+
+  /* USER CODE END FDCAN3_Init 0 */
+
+  /* USER CODE BEGIN FDCAN3_Init 1 */
+
+  /* USER CODE END FDCAN3_Init 1 */
+  hfdcan3.Instance = FDCAN3;
+  hfdcan3.Init.ClockDivider = FDCAN_CLOCK_DIV1;
+  hfdcan3.Init.FrameFormat = FDCAN_FRAME_CLASSIC;
+  hfdcan3.Init.Mode = FDCAN_MODE_NORMAL;
+  hfdcan3.Init.AutoRetransmission = DISABLE;
+  hfdcan3.Init.TransmitPause = DISABLE;
+  hfdcan3.Init.ProtocolException = DISABLE;
+  hfdcan3.Init.NominalPrescaler = 4;
+  hfdcan3.Init.NominalSyncJumpWidth = 1;
+  hfdcan3.Init.NominalTimeSeg1 = 15;
+  hfdcan3.Init.NominalTimeSeg2 = 4;
+  hfdcan3.Init.DataPrescaler = 2;
+  hfdcan3.Init.DataSyncJumpWidth = 1;
+  hfdcan3.Init.DataTimeSeg1 = 15;
+  hfdcan3.Init.DataTimeSeg2 = 4;
+  hfdcan3.Init.StdFiltersNbr = 1;
+  hfdcan3.Init.ExtFiltersNbr = 0;
+  hfdcan3.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
+  if (HAL_FDCAN_Init(&hfdcan3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN FDCAN3_Init 2 */
+
+  /* USER CODE END FDCAN3_Init 2 */
+
+}
+
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 79;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 999;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pins : motor0_upper_Pin motor1_upper_Pin */
+  GPIO_InitStruct.Pin = motor0_upper_Pin|motor1_upper_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : motor0_lower_Pin motor1_lower_Pin */
+  GPIO_InitStruct.Pin = motor0_lower_Pin|motor1_lower_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
